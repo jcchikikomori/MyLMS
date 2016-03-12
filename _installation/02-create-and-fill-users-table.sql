9cc3a374b833b8c38c84ec636010e200b5c6a8bd
jccultima123 <johncyrillcorsanes@gmail.com>
2016-03-12 18:13:32 +0800

    [FIX] Several updated on DB scripts

--- _installation/02-create-and-fill-users-table.sql
+++ _installation/02-create-and-fill-users-table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `test_lms`.`user_types` (
-  `id` int(11) NOT NULL,
+  `id` int(11) PRIMARY KEY NOT NULL,
   `user_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `type_desc` varchar(300) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Full information about this user type'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `test_lms`.`users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
-  `user_type` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `user_type` int(11) COLLATE utf8_unicode_ci NOT NULL,
   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
   `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
